68717cb6bdda85b551438a245dbc46707c22d331
Carlos Bernal <22770674+Maxinfamily@users.noreply.github.com>
2021-01-24 22:30:26 +0000

    more changes exercise 2

--- ExploreCalifornia/ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/ExploreCalifornia/Controllers/BlogController.cs
@@ -8,13 +8,14 @@ namespace ExploreCalifornia.Controllers
 {
     public class BlogController : Controller
     {
+        [Route("blog")] //Doesnt apply to actions without Route attribute
         // GET: /<controller>/
         public IActionResult Index()
         {
             return new ContentResult { Content = "Blog posts" };
         }
 
-        [Route("blog/{year:min(2000)}/{month:range(1,12)}/{key}")]
+        [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post (int year, int month, string key)
         {
             return new ContentResult { Content = string.Format("Year: {0}; Month: {1}; Key: {2}", year, month, key) };
