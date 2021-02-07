dcde40f3b18078a1b93345d73d981b171b1bdfdb
Carlos Bernal <22770674+Maxinfamily@users.noreply.github.com>
2021-02-07 15:44:23 +0000

    Added missing files to repo

--- ExploreCalifornia/ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/ExploreCalifornia/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExploreCalifornia.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,14 +13,69 @@ namespace ExploreCalifornia.Controllers
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return new ContentResult { Content = "Blog posts" };
+            //return new ContentResult { Content = "Blog posts" };
+
+            var posts = new[]
+            {
+                new Post
+                {
+                    Title = "My blog post",
+                    Posted = DateTime.Now,
+                    Author = "Jess Chadwick",
+                    Body = "This is a great blog post, don't you think?",
+                },
+                new Post
+                {
+                    Title = "My second blog post",
+                    Posted = DateTime.Now,
+                    Author = "Jess Chadwick",
+                    Body = "This is ANOTHER great blog post, don't you think?",
+                },
+            };
+
+            return View(posts);
         }
 
         [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
-        public IActionResult Post (int year, int month, string key)
+        public IActionResult Post(int year, int month, string key)
         {
-            return new ContentResult { Content = string.Format("Year: {0}; Month: {1}; Key: {2}", year, month, key) };
+            var post = new Post
+            {
+                Title = "My blog post",
+                Posted = DateTime.Now,
+                Author = "Jess Chadwick",
+                Body = "This is a great blog post, don't you think?",
+            };
+
+            return View(post);
         }
+
+        [Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+
+        //[Route(@"{year:min(2000)}/{month:range(1,12)}/{key}")]
+        //public IActionResult Post(int year, int month, string key)
+        //{
+        //    ViewBag.Title = "My blog post";
+        //    ViewBag.Posted = DateTime.Now;
+        //    ViewBag.Author = "Jess Chadwick";
+        //    ViewBag.Body = "This is a great blog post, don't you think?";
+
+        //    return View();
+        //}
+
+        //[Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
+        //public IActionResult Post (int year, int month, string key)
+        //{
+        //    //return new ContentResult { Content = string.Format("Year: {0}; Month: {1}; Key: {2}", year, month, key) };
+
+        //    return View();
+        //}
 
         //public IActionResult Post(int? id)
         //{
